cbb5180179bcb2e42ae93944c94fcf4328b2fb79
Umar Khan <uk4495848@gmail.com>
2024-08-28 11:22:48 +0500

    react native carousel

---
+++ App.js
@@ -0,0 +1,18 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Slider from './src/components/Slider'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+    <Slider/>
+    
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+
+
+
---
+++ src/data/index.js
@@ -0,0 +1,37 @@
+export default [
+{
+    id:1,
+    img: require('../Images/watch1.png'),
+    title:'watch1',
+    description:'this is first watch',
+    price:'$200'
+},
+{
+    id:2,
+    img: require('../Images/watch2.png'),
+    title:'watch2',
+    description:'this is second watch',
+    price:'$300'
+},
+{
+    id:3,
+    img: require('../Images/watch3.png'),
+    title:'watch3',
+    description:'this is third watch',
+    price:'$400'
+},
+{
+    id:4,
+    img: require('../Images/watch4.png'),
+    title:'watch4',
+    description:'this is forth watch',
+    price:'$500'
+},
+{
+    id:5,
+    img: require('../Images/watch5.png'),
+    title:'watch5',
+    description:'this is fifth watch',
+    price:'$600'
+},
+]
